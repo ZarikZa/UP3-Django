4ed7b73e945aa948cba76a83993c384f8d8b46a6
ZarikZa <isip_ya.d.shemetov@mpt.ru>
2025-06-13 03:46:46 +0300

    3 but logo ne appears

--- shopproject/shop/forms.py
+++ shopproject/shop/forms.py
@@ -4,43 +4,66 @@ from .models import *
 class ClothesForm(forms.ModelForm):
     class Meta:
         model = Clothes
-        #Все поля которые нужно заполнять
         fields = ['name', 'disctiption', 'price', 'size', 'color', 'photo', 'is_exists', 'category', 'collection']
+
 
 class ProductsForm(forms.ModelForm):
     class Meta:
         model = Products
-        #Все поля которые нужно заполнять
         fields = ['name', 'disctiption', 'price', 'photo', 'category', 'country', 'brand']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'disctiption': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+            'country': forms.Select(attrs={'class': 'form-select'}),
+            'brand': forms.Select(attrs={'class': 'form-select'}),
+        }
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        # Все поля которые нужно заполнять
         fields = ['name', 'disctiption', 'photo']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'disctiption': forms.Textarea(attrs={'class': 'form-control'}),
-            'photo': forms.FileInput(attrs={'class': 'form-control-file'}),
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
         }
 
 class CountryProivodstvaForm(forms.ModelForm):
     class Meta:
         model = CountryProivodstva
-        # Все поля которые нужно заполнять
         fields = ['country']
+        widgets = {
+            'country': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class BrandForm(forms.ModelForm):
     class Meta:
         model = Brand
         fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class ClentsForm(forms.ModelForm):
     class Meta:
         model = Clients
         fields = ['clientName', 'clientSurname', 'clientMiddleName', 'login', 'password']
+        widgets = {
+            'clientName': forms.TextInput(attrs={'class': 'form-control'}),
+            'clientSurname': forms.TextInput(attrs={'class': 'form-control'}),
+            'clientMiddleName': forms.TextInput(attrs={'class': 'form-control'}),
+            'login': forms.TextInput(attrs={'class': 'form-control'}),
+            'password': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
 
 class WishlistForm(forms.ModelForm):
     class Meta:
         model = Wishlist
         fields = ['client', 'product']
+        widgets = {
+            'client': forms.Select(attrs={'class': 'form-select'}),
+            'product': forms.Select(attrs={'class': 'form-select'}),
+        }
